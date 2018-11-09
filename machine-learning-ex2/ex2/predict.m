6594e301725f504549b2dbdd63b7c17154825bd1
Abdul Samad <abdulsamad@TetraNoodle.local>
2018-11-10 00:59:18 +0200

    Added prediction for future inputs.

--- machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/ex2/predict.m
@@ -15,13 +15,13 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
-
-
-
-
-
-
-% =========================================================================
-
+hyp = X*theta;
+sigmoidHyp = sigmoid(hyp);
+for i = 1:m
+    if sigmoidHyp(i) >= 0.5
+        p(i) = 1;
+    else
+        p(i) = 0;
+    end
 
 end
